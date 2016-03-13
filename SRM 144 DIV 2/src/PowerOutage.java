d647da2b55a048f69106aa2d59b48abc013bcc9b
andriybuday <andriybuday@gmail.com>
2016-03-13 20:43:22 +0100

    Added generated poweroutage cases

---
+++ SRM 144 DIV 2/src/PowerOutage.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import java.math.*;
+import static java.lang.Math.*;
+
+public class PowerOutage {
+	
+	public int estimateTimeOut(int[] fromJunction, int[] toJunction, int[] ductLength) {
+		return 0;
+	}
+}
